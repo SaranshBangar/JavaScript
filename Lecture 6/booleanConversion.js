03ff13025bb7ceb016c524aebdfa20dfa70f3e55
Saransh Bangar <saranshbangad@gmail.com>
2023-10-17 21:51:30 +0530

    Added JS codes for lecture 4, 5 and 6

---
+++ Lecture 6/booleanConversion.js
@@ -0,0 +1,15 @@
+let str1 = ""
+let str1Value = Boolean(str1)
+console.log(typeof str1Value)
+console.log(str1Value)
+// boolean
+// false
+
+console.log("\n") // Prints new line
+
+let str2 = "Saransh"
+let str2Value = Boolean(str2)
+console.log(typeof str2Value)
+console.log(str2Value)
+// boolean
+// true
