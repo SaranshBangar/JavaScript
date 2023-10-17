fc56c81bd3dcfcf8cbd7006910c022548368c97a
Saransh Bangar <saranshbangad@gmail.com>
2023-10-17 21:56:08 +0530

    Update variables.js

--- Lecture 4/variables.js
+++ Lecture 4/variables.js
@@ -4,6 +4,6 @@ const accountId = 10 // "const" keyword prevents the value of the variable from 
 let accountEmail = "saranshbangad@gmail.com"
 var accountPassword = "1234" // It is not used because of block and functional scope
 accountCity = "Gurugram" // Not preffered by programmers
-let accountState // It is assigned "undefined" value
+let accountState // It is alloted "undefined" value
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) // Prints all of the variables in a tabular format
