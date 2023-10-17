fa196bd886a4341562cd784cb3c2c65ee2079567
SaranshBangar <saranshbangad@gmail.com>
2023-10-17 21:59:17 +0530

    Added new case in booleanConversion.js

--- Lecture 6/booleanConversion.js
+++ Lecture 6/booleanConversion.js
@@ -13,3 +13,12 @@ console.log(typeof str2Value)
 console.log(str2Value)
 // boolean
 // true
+
+console.log("\n") // Prints new line
+
+let str3 = null
+let str3Value = Boolean(str3)
+console.log(typeof str3Value)
+console.log(str3Value)
+// boolean
+// false
