eecc402dff47984d90633ab6fddb132514af3474
SaranshBangar <saranshbangad@gmail.com>
2023-10-18 15:37:11 +0530

    Added code for lecture 7

---
+++ Lecture 7/operations.js
@@ -0,0 +1,25 @@
+// Turning a value into negative
+let value = 2
+let negValue = -value
+console.log(negValue)
+// -2
+
+// Basic mathematical operations in JavaScript
+console.log(2+3) // 5
+console.log(2-3) // -1
+console.log(2*3) // 6
+console.log(2/3) // 0.66
+console.log(2**3) // 8
+console.log(2%3) // 2
+console.log(++value) // 3
+
+// Basic string concatonation
+let str1 = "Hello"
+let str2 = " Saransh"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
