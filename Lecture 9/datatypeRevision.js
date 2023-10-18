a78673027fa7de938cf1a013e7dffe4bd58c8e46
SaranshBangar <saranshbangad@gmail.com>
2023-10-18 17:06:40 +0530

    Added code for lecture 9

---
+++ Lecture 9/datatypeRevision.js
@@ -0,0 +1,10 @@
+// There are 2 types of datatype -
+// Primitive (7) -> String, Number, Boolean, Null, Undefined, Symbol, BigInt
+// Non Primitive a.k.a. Reference (3) -> Array, Objects, Functions
+
+// Note - JavaScript is a dynamically typed language
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+console.log(typeof Symbol) // function
+console.log(typeof Object) // function
