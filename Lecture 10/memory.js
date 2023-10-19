fe1f49fff440df4a82c0df454ca03117126abf89
SaranshBangar <saranshbangad@gmail.com>
2023-10-19 14:43:25 +0530

    Added part 1 of Lecture 10 code

---
+++ Lecture 10/memory.js
@@ -0,0 +1,7 @@
+// Memory is of 2 types - Stack (Primitive) and Heap (Non - Primitive)
+
+let name1 = "Saransh" // Initiated "name1" and gave it the value "Saransh"
+let name2 = name1 // Initiated "name2" and gave it the value of "name1"
+name2 = "Bangar" // Updated "name2"s value to "Bangar"
+console.log(name1) // Saransh
+console.log(name2) // Bangar
