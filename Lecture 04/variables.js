980f2e8d4abc2297998f998e28e4b36111913f63
SaranshBangar <saranshbangad@gmail.com>
2023-10-19 14:33:17 +0530

    Updated folder's name

---
+++ Lecture 04/variables.js
@@ -0,0 +1,9 @@
+// There are 3 ways to declare a variable and 1 way to declare a constant
+
+const accountId = 10 // "const" keyword prevents the value of the variable from changing throughout the code
+let accountEmail = "saranshbangad@gmail.com"
+var accountPassword = "1234" // It is not used because of block and functional scope
+accountCity = "Gurugram" // Not preffered by programmers
+let accountState // It is alloted "undefined" value
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) // Prints all of the variables in a tabular format
---
+++ Lecture 06/stringConversion.js
@@ -0,0 +1,33 @@
+let num = "30"
+let numValue = String(num)
+console.log(typeof numValue)
+console.log(numValue)
+// string
+// 30
+
+console.log("\n") // Prints new line
+
+let bool = true
+let boolValue = String(bool)
+console.log(typeof boolValue)
+console.log(boolValue)
+// string
+// true
+
+console.log("\n") // Prints new line
+
+let Null = null
+let NullValue = String(null)
+console.log(typeof NullValue)
+console.log(NullValue)
+// string
+// null
+
+console.log("\n") // Prints new line
+
+let und = undefined
+let undValue = String(und)
+console.log(typeof undValue)
+console.log(undValue)
+// string
+// undefined
