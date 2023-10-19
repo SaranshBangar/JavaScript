2a4fac6d743f823c9e4a7326c33c16a01758cf41
SaranshBangar <saranshbangad@gmail.com>
2023-10-19 16:42:05 +0530

    Lecture 11 code and notes (part - 1)

---
+++ Lecture 11/string.js
@@ -0,0 +1,11 @@
+// String concatonation (Old method)
+const str1 = "Hello "
+const str2 = "World"
+console.log(str1 + str2)
+
+// String interpolation (New method)
+const name = "Saransh"
+console.log(`Hello, my name is ${name}`);
+
+const userName = new String("Saransh Bangar")// Another way to declare string using object
+// The above method is much more beneficial as we get many functions (like ".length()") that can work with it
