fd6cad7102132137115337f091f4e7ce00ae42b1
SaranshBangar <saranshbangad@gmail.com>
2023-10-28 21:17:44 +0530

    Lecture 20 code and notes (Part 1)

---
+++ Lecture 20/functions2.js
@@ -0,0 +1,5 @@
+function calculateShoppingCartPrice(...price1) // Rest parameter // Note -> It looks the same as spread operator but it differs in use case and syntax
+{
+    return price1 
+}
+console.log(calculateShoppingCartPrice(100, 200, 500)) // [ 100, 200, 500 ]
